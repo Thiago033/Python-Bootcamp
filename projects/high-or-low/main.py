666a073ef06aa95c6d5dfefba989d47bbafd0a3a
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2022-12-04 17:51:54 -0300

    Update main.py

--- projects/high-or-low/main.py
+++ projects/high-or-low/main.py
@@ -6,54 +6,57 @@ from gamedata import data
 clear = lambda: os.system('cls')
 
 
-def getRandomAccount():
+def get_random_account():
     return random.choice(data)
 
-def formatData(account):
+
+def format_data(account):
     name = account["name"]
     description = account["description"]
     country = account["country"]
-    
+
     return f"{name}, a {description}, from {country}"
 
 
-def checkAnswer(guess, A, B):
-  if A > B:
-    return guess == "a"
-  else:
-    return guess == "b"
+def check_answer(guess, a, b):
+    if a > b:
+        return guess == "a"
+    else:
+        return guess == "b"
+
 
 def game():
     print(logo)
-    
+
     score = 0
-    continueGame = True
-    
-    accountB = getRandomAccount()
-    
-    while continueGame:
-        
-        accountA = accountB
-        accountB = getRandomAccount()
-        
-        print(f"Compare A: {formatData(accountA)}.")
+    continue_game = True
+
+    account_b = get_random_account()
+
+    while continue_game:
+
+        account_a = account_b
+        account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}.")
         print(vs)
-        print(f"Against B: {formatData(accountB)}.")
-        
+        print(f"Against B: {format_data(account_b)}.")
+
         guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-        
-        folowsA = accountA["follower_count"]
-        folowsB = accountB["follower_count"]
-        
-        isCorrect = checkAnswer(guess, folowsA, folowsB)
-        
+
+        follows_a = account_a["follower_count"]
+        follows_b = account_b["follower_count"]
+
+        is_correct = check_answer(guess, follows_a, follows_b)
+
         clear()
         print(logo)
-        if isCorrect:
+        if is_correct:
             score += 1
             print(f"You're right! Current score: {score}.")
         else:
-            continueGame = False
+            continue_game = False
             print(f"Sorry, that's wrong. Final score: {score}")
-        
+
+
 game()
